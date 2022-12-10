b1ca6f0330be5d524e64559bed672d7cbf439d95
Lee Taylor <lees_taylor_18@yahoo.com>
2022-12-10 20:22:53 +0000

    Joined norms as hinted to

--- main.py
+++ main.py
@@ -13,12 +13,28 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
 # Load houses dataset
-df = pd.read_csv("houses.csv")
+df = pd.read_csv("houses.csv", header=0)
 print(f"{df.shape}\n\n{df.columns}")
 
-# Print correlation matrix
-corr = df.corr()
-print(corr)
+''' A number of the features are such things as 'total_rooms' and 'total_bedrooms'.
+This is for all houses in the area. As the size of the areas may be different it
+is worth normalising these based on the number of properties in the area. 
+[Drop column example ->] X_train_ = imputed_dataset.copy().drop([target, 'index'], axis=1) '''
+
+# Normalize columns
+total_rooms_norm    = df['total_rooms'] / df['households']
+total_bedrooms_norm = df['total_bedrooms'] / df['households']
+
+'''
+        >>> df = pd.DataFrame({'key': ['K0', 'K1', 'K2', 'K3', 'K4', 'K5'],
+        ...                    'A': ['A0', 'A1', 'A2', 'A3', 'A4', 'A5']})
+'''
+# Modify dataset
+norms = pd.DataFrame({'total_rooms_norm'    : total_rooms_norm,
+                      'total_bedrooms_norm' : total_bedrooms_norm})
+df = df.join(norms)
+df = df.drop(columns=['total_rooms', 'total_bedrooms'])
+print(f"{df.shape}\n\n{df.columns}")
 
 
 # np.random.seed(42)
