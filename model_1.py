7f8534ff3fe9471b37c9ca7426f7a926415d1398
Lee Taylor <lees_taylor_18@yahoo.com>
2022-12-11 00:18:21 +0000

    Created 4 models, logistic regression, SVM, Deep Neural Network, TPOT Genetic Programming

---
+++ model_1.py
@@ -0,0 +1,25 @@
+# Author: Lee Taylor, ST Number: 190211479
+from pre_processing import *
+from sklearn.linear_model       import LinearRegression
+from sklearn.model_selection    import KFold
+from sklearn.metrics            import r2_score
+from sklearn.metrics            import mean_squared_error
+
+
+# Init. train/test datasets
+X_train, X_test, y_train_true, y_test_true = gen_data()
+
+# Create model
+np.random.seed(100)
+m1 = LinearRegression()
+
+# Fit model
+m1.fit(X_train.values, y_train_true.values)
+
+# Test model
+y_test_pred = m1.predict(X_test.values)
+print(f"-------------------------------")
+print("MSE: {0:.3}".format(mean_squared_error(y_test_true, y_test_pred)))
+print("R^2: {0:.2}".format(r2_score(y_test_true, y_test_pred)))
+print(f"-------------------------------")
+
---
+++ pre_processing.py
@@ -0,0 +1,37 @@
+# Author: Lee Taylor, ST Number: 190211479
+import numpy  as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import scale
+
+
+# Configure pandas
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
+# Load houses dataset
+df = pd.read_csv("houses.csv", header=0)
+print(f"{df.shape}\n\n{df.columns}")
+
+# Normalize columns
+total_rooms_norm    = df['total_rooms'] / df['households']
+total_bedrooms_norm = df['total_bedrooms'] / df['households']
+
+# Modify dataset
+norms = pd.DataFrame({'total_rooms_norm'    : total_rooms_norm,
+                      'total_bedrooms_norm' : total_bedrooms_norm})
+df = df.join(norms)
+df = df.drop(columns=['total_rooms', 'total_bedrooms'])
+print(f"{df.shape}\n\n{df.columns}")
+
+def gen_data():
+    """ Return Train-Test datasets """
+    X = df.copy().drop(columns=['median_house_value'], axis=1)
+    y = df['median_house_value']
+    np.random.seed(42)
+    X_train, X_test, y_train_true, y_test_true = train_test_split(X, y, test_size=0.33, random_state=42)
+    return X_train, X_test, y_train_true, y_test_true
+
+
+if __name__ == '__main__':
+    pass
