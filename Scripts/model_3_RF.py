5edf16723e3dae15ae84f84eac3fccd5ecdf6f1b
Lee <lees_taylor_18@yahoo.com>
2022-12-20 00:21:10 +0000

    Model 3 results

--- Scripts/model_3_RF.py
+++ Scripts/model_3_RF.py
@@ -6,11 +6,24 @@ from    functions_ import *
 # Define the datasets
 X_train, X_test, y_train, y_test = gen_data(debug=False)
 
+''' First group of hyperparams to test '''
+# # Define the hyperparameters to search over
+# hyperparams = {"n_estimators": [10, 50, 100, 200, 300],
+#                "max_depth": [None, 10, 20, 30, 40, 50],
+#                "min_samples_split": [2, 5, 10, 15, 20],
+#                "min_samples_leaf": [1, 2, 4, 8]}
+''' Second group of hyperparams to test '''
 # Define the hyperparameters to search over
-hyperparams = {"n_estimators": [10, 50, 100, 200, 300],
-               "max_depth": [None, 10, 20, 30, 40, 50],
-               "min_samples_split": [2, 5, 10, 15, 20],
-               "min_samples_leaf": [1, 2, 4, 8]}
+hyperparams = {"n_estimators": [300],
+               "max_depth": [None],
+               "min_samples_split": [2],
+               "min_samples_leaf": [2],
+               "bootstrap": [True],
+               "oob_score": [True, False],
+               "warm_start": [True, False],
+               "min_impurity_decrease": [0, 0.1, 0.2, 0.3, 0.4, 0.5],
+               "ccp_alpha": [0, 0.1, 0.2, 0.3, 0.4, 0.5]
+               }
 
 # Perform the grid search
 best_params, best_score, results = grid_search(RandomForestRegressor, hyperparams,
@@ -18,7 +31,7 @@ best_params, best_score, results = grid_search(RandomForestRegressor, hyperparam
                                                verbose=True)
 
 # Write the results to a CSV file
-csvwriter_(results, "3", "1")
+csvwriter_(results, "3", "3")
 
 # Print the results
 print()
